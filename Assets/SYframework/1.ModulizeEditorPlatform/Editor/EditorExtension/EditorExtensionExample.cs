d6bf502294ae3be197909aef4d9b16d8aa1062dd
ghliqing <1214392081@qq.com>
2021-11-17 20:15:14 +0800

    编辑器扩展v0.0.2
    编辑器扩展 GUI以及GUILayout,GUIUtitly

--- Assets/SYframework/1.ModulizeEditorPlatform/Editor/EditorExtension/EditorExtensionExample.cs
+++ Assets/SYframework/1.ModulizeEditorPlatform/Editor/EditorExtension/EditorExtensionExample.cs
@@ -20,13 +20,20 @@ namespace SYFramework
 		enum PageID
 		{
 			BasePanel,
+			EnablePanel,
+			RotaPanel,
+			Scale,
+			Color,
 			TwoPanel,
 
 		}
 
 		private PageID currentSelect;
+
+		private bool pageEnable=true;
 		private void OnGUI()
 		{
+			
 			//转换
 			currentSelect = (PageID)GUILayout.Toolbar((int)currentSelect,Enum.GetNames(typeof(PageID)));
 
@@ -35,15 +42,93 @@ namespace SYFramework
 				//
 				Base();
 			}
+			else if (currentSelect == PageID.EnablePanel)
+			{
+				Enable();
+			}
+			else if (currentSelect==PageID.RotaPanel)
+			{
+				RotaPanel();
+
+			}
+			else if (currentSelect == PageID.Scale)
+			{
+				Scale();
+			}
+			else if (currentSelect == PageID.Color)
+			{
+				ChangeColor();
+			}
 			else if (currentSelect == PageID.TwoPanel)
 			{
 				TwoPanel();
 			}
 		}
 
+		#region Color
+		private bool colorbool;
+
+		private void ChangeColor()
+		{
+			colorbool = GUILayout.Toggle(colorbool,"改变颜色");
+			if (colorbool)
+			{
+				GUI.color = Color.green;
+			}
+			TwoPanel();
+		}
+		#endregion
+
+		#region Scale
+		private bool scaleBool;
+
+		private void Scale()
+		{
+			scaleBool=GUILayout.Toggle(scaleBool, "缩放");
+			if (scaleBool)
+			{
+				GUIUtility.ScaleAroundPivot(Vector2.one*0.1f, Vector2.one*200f);
+			}
+
+			TwoPanel();
+		}
+
+		#endregion
+
+		#region Rota
+
+		private bool rotaPanelbool;
+		private void RotaPanel()
+		{
+			rotaPanelbool = GUILayout.Toggle(rotaPanelbool, "旋转");
+			if (rotaPanelbool)
+			{
+				GUIUtility.RotateAroundPivot(45, Vector2.one);
+			}
+			Base();
+
+		}
+		#endregion
+
+
+		#region Enable
+		private void Enable()
+		{
+			pageEnable = GUILayout.Toggle(pageEnable, "是否交互第二个面板");
+
+			if (GUI.enabled!=pageEnable)
+			{
+				GUI.enabled = pageEnable;
+			}
+		
+			Base();
+		}
+
+		#endregion
+
 		#region Base
 
-		
+
 		private void Base()
 		{
 
@@ -51,6 +136,9 @@ namespace SYFramework
 			{
 				GUILayout.Label("按钮");
 				GUILayout.Button("Btn");
+
+				GUILayout.Box("Box",new GUIStyle() { fixedHeight=100,fixedWidth=200});
+
 			}
 			GUILayout.EndVertical();
 			
@@ -88,13 +176,14 @@ namespace SYFramework
 				GUILayout.BeginHorizontal();
 				{
 					GUILayout.Label("输入密码：");
-					userPassword = GUILayout.PasswordField(userPassword,'*');
+					userPassword = GUILayout.PasswordField(userPassword, '*');
 
 				}
-				GUILayout.EndHorizontal ();
+				GUILayout.EndHorizontal();
 
 				GUILayout.Label("toggle:");
-				toggleGUI=GUILayout.Toggle(toggleGUI,"Toggle");
+
+				toggleGUI = GUILayout.Toggle(toggleGUI, "Toggle");
 
 				GUILayout.Label("滑动条：");
 				hSlider = GUILayout.HorizontalSlider(0, 100, 0);
@@ -106,7 +195,20 @@ namespace SYFramework
 				GUILayout.Label("txtarea:");
 				txtArea = GUILayout.TextArea(txtArea);
 
+				GUILayout.BeginHorizontal();
+				{
+					GUILayout.Label("Button:");
+					
+					if (GUILayout.Button("按钮"))
+					{
+						Debug.Log("点击");
+					}
+				}
+				GUILayout.EndHorizontal();
 				
+			
+
+
 			}
 			GUILayout.EndVertical();
 
